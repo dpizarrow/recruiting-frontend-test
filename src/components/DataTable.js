92a3e11dd5ee82ded6fc5a9cc5b0f24949bc382d
dpizarrow <diego.pizarro0@gmail.com>
2024-04-25 12:14:30 -0400

    chore: remove table header

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -25,17 +25,14 @@ function DataTable({ data, usdToClp, selectedRow, setSelectedRow }) {
     return (
       <div className="overflow-x-auto">
         <table className="min-w-full table-auto">
-          <thead className="bg-gray-800 text-white">
-            <tr>
-              <th className="px-4 py-2"></th>
-              <th className="px-4 py-2">ID</th>
-              <th className="px-4 py-2">Amount</th>
-              <th className="px-4 py-2">Type</th>
-            </tr>
-          </thead>
           <tbody>
             {data.map((item) => (
-              <tr key={item.id} className="text-center bg-gray-100 border-b">
+              <tr
+                key={item.id}
+                className={`text-center bg-gray-100 border-b ${
+                  selectedRow === item.id ? "bg-violet-100 text-indigo-700" : ""
+                }`}
+              >
                 <td className="px-4 py-2">
                   <input
                     type="radio"
