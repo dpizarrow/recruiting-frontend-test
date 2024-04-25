6d1c14b3dfc9aa90940ed84e77df2e6619da7881
dpizarrow <diego.pizarro0@gmail.com>
2024-04-25 10:55:06 -0400

    feat: create state for invoices and credit notes

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import apiClient from "./api/apiClient";
 import DataTable from "./components/DataTable";
 
 function App() {
-  const [data, setData] = useState([]);
+  const [invoices, setInvoices] = useState([]);
+  const [creditNotes, setCreditNotes] = useState([]);
 
   useEffect(() => {
     apiClient
       .get("/invoices/pending")
       .then((response) => response.data)
       .then((data) => {
-        setData(data);
+        setInvoices(data.filter((item) => item.type === "received"));
+        setCreditNotes(data.filter((item) => item.type === "credit_note"));
       });
-  });
+  }, []);
+
+  console.log("invoices", invoices);
+  console.log("creditNotes", creditNotes);
 
   const usdToClp = 949; // This is a mock value, we could fetch the real value from an API
 
@@ -22,7 +27,7 @@ function App() {
         Selecciona una factura
       </h1>
       <div className="px-8 pt-6 pb-8 mb-4">
-        <DataTable data={data} usdToClp={usdToClp} />
+        <DataTable data={invoices} usdToClp={usdToClp} />
       </div>
     </div>
   );
--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -14,21 +14,21 @@ function DataTable({ data, usdToClp }) {
           <tr>
             <th className="px-4 py-2">ID</th>
             <th className="px-4 py-2">Amount</th>
-            <th className="px-4 py-2">Currency</th>
-            <th className="px-4 py-2">Organization ID</th>
             <th className="px-4 py-2">Type</th>
           </tr>
         </thead>
         <tbody>
           {data.map((item) => (
             <tr key={item.id} className="text-center bg-gray-100 border-b">
-              <td className="px-4 py-2">{item.id}</td>
+              <td className="px-4 py-2">
+                {item.id} ({item.organization_id})
+              </td>
               <td className="px-4 py-2">
                 {formatAmount(item.amount, item.currency)}
               </td>
-              <td className="px-4 py-2">{item.currency}</td>
-              <td className="px-4 py-2">{item.organization_id}</td>
-              <td className="px-4 py-2">{item.type}</td>
+              <td className="px-4 py-2">
+                {item.type === "received" ? "Recibida" : item.reference}
+              </td>
             </tr>
           ))}
         </tbody>
